b9ea64e93cdf2edb144e3d29db7ccc1fc4e568a4
acathers <37058209+acathers@users.noreply.github.com>
2019-04-04 03:51:12 -1000

    Update Landing.jsx
    --add buttons to own row for alignment
    --removed unused imports

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Search, Button, Grid, Header, Container } from 'semantic-ui-react';
+import { Button, Grid, Header, Container } from 'semantic-ui-react';
 
 class Landing extends React.Component {
 
@@ -7,8 +7,8 @@ class Landing extends React.Component {
     return (
         <div style={{ width: '100%' }}>
           <Container fluid style={{ minHeight: '50vh' }} className={'backgroundimage'}></Container>
-          <Grid relaxed container columns={3} style={{ paddingTop: '10px', minWidth: '90%' }}>
-            <Grid.Column textAlign='center'>
+          <Grid container textAlign='center' columns={3} style={{ paddingTop: '10px', minWidth: '90%' }}>
+            <Grid.Column>
               <Header as={'h2'}>Mission</Header>
               <p>The University of Hawaii at Manoa currently has over 200 registered independent organizations. We are
                 hoping to give these clubs a place to call home. And with
@@ -19,14 +19,22 @@ class Landing extends React.Component {
               <Header as={'h2'}>Browse and Join clubs!</Header>
               <p>After registering you will have access to the full database of UH Manoa clubs. You can browse and join
                 clubs at your leisure </p>
-              <Button size={'massive'} inverted>Sign up</Button>
             </Grid.Column>
             <Grid.Column textAlign='center'>
               <Header as={'h2'}>Club Owners</Header>
               <p>Are you looking for a place to give your club presence? Look no further! simply register your club and
                 you will be able to manage it easily!</p>
-              <Button size={'massive'} inverted>Create Club</Button>
             </Grid.Column>
+            <Grid.Row>
+              <Grid.Column>
+              </Grid.Column>
+              <Grid.Column>
+                <Button size={'massive'} inverted>Sign up</Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Button size={'massive'} inverted>Create Club</Button>
+              </Grid.Column>
+            </Grid.Row>
           </Grid>
         </div>
     );
