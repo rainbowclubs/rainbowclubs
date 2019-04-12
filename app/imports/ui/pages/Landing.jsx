d00aece141efdd34f8178c1e0ee62e6c2842d2db
acathers <37058209+acathers@users.noreply.github.com>
2019-04-11 22:50:24 -1000

    issue-11-final-3

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Image, Segment, Header, Grid, Container } from 'semantic-ui-react';
 
-/** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
+
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div>
+          <Container textAlign='center' className={'backgroundImage'} fluid style={{ minHeight: '50vh' }}>
+            <Container style={{ paddingTop: '7%' }}>
+              <p style={{ fontWeight: 'bold', color: 'white', fontSize: '40px' }}>The University of Hawaii at Manoa
+                currently has over
+                200 registered
+                independent organizations. We are
+                hoping to give these clubs a place to call home. And with
+                that a central area where students can easily browse and join the many unique and exhilarating clubs
+                that University of Hawaii at Manoa has to offer.</p>
+            </Container>
+          </Container>
+          <Container fluid style={{ paddingTop: '20px' }}>
+            <Grid columns={'equal'} padded>
+              <Grid.Column>
+                <Image centered fluid style={{ maxHeight: '30vh' }} src={'images/club1.jpg'}/>
+              </Grid.Column>
+              <Grid.Column>
+                <Image centered fluid style={{ maxHeight: '30vh' }} src={'images/club2.jpg'}/>
+              </Grid.Column>
+              <Grid.Column>
+                <Image centered fluid style={{ maxHeight: '30vh' }} src={'images/club3.jpg'}/>
+              </Grid.Column>
+            </Grid>
+          </Container>
+          <Container>
+            <Grid columns={2} centered>
+              <Grid.Column style={{ paddingTop: '10%' }} verticalAlign='center' textAlign={'center'}>
+                <Segment className={'UHGreenBackground'}> <Header inverted as={'h2'}>Easily browse the registerd clubs
+                  of UH Manoa by
+                  name and
+                  category!</Header></Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Image src={'images/clubs.jpg'}/>
+              </Grid.Column>
+            </Grid>
+          </Container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        </div>
     );
   }
 }
