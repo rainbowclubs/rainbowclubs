ca5aa77366f6e6804d45b98ec68858201d138e7c
Audrey Ford <ariding@hawaii.edu>
2019-04-11 19:55:55 -1000

    issue-2-final-3

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -7,10 +7,12 @@ class Footer extends React.Component {
     return (
         <footer>
           <div style={divStyle} className="ui center aligned container">
+            <div style={divStyle} className='UHGreenTextColor'>
             <hr />
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822
+            </div>
           </div>
         </footer>
     );
