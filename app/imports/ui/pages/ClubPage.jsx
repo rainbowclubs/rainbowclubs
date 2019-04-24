c4aa79684459d4ea1445e0fe7ccc1aa6a16fd8ec
Leland Machii <lelandmachii@gmail.com>
2019-04-23 21:14:31 -1000

    Issue-19-final
    Unformatted page of clubs

--- app/imports/ui/pages/ClubPage.jsx
+++ app/imports/ui/pages/ClubPage.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader } from 'semantic-ui-react';
-import { Clubs } from '/imports/api/club/club';
-// import Club from '/imports/ui/components/ClubItem';
+import { Container, Header, Loader, Divider, Grid } from 'semantic-ui-react';
+import { Clubs, ClubSchema } from '/imports/api/club/club';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 
@@ -17,25 +16,60 @@ class ClubPage extends React.Component {
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   renderPage() {
     return (
-        <Container>
-          <Header className={'Sign'} as="h2" textAlign="center">Club Information</Header>
-        </Container>
+        <Container schema={ClubSchema} model={this.props.doc}>
+          <Header className={'Sign'} as="h2" textAlign="center">{this.props.doc.name}</Header>
+          <div>
+            <Grid centered columns={'equal'}>
+              <Grid.Column>
+                <Container>
+                  <b>Contact:</b> {this.props.doc.contactName}
+                </Container>
+                <Container>
+                  <b>Email:</b> {this.props.doc.contactEmail}
+                </Container>
+              </Grid.Column>
+              <Grid.Column>
+                  <Container textAlign={'center'}>
+                    <b>Types:</b> {this.props.doc.types}
+                  </Container>
+              </Grid.Column>
+              <Grid.Column>
+                <Container textAlign={'right'}>
+                  <b>Club Email:</b> {this.props.doc.clubEmail}
+                </Container>
+                <Container textAlign={'right'}>
+                  <b>Club Website:</b> {this.props.doc.clubEmail}
+                </Container>
+              </Grid.Column>
+            </Grid>
+            <Container textAlign='justified'>
+              <b>Description:</b>
+              <Divider />
+              <p>
+                {this.props.doc.description}
+              </p>
+            </Container>
+          </div>
+          </Container>
     );
   }
 }
 
 /** Require the presence of a Club document in the props object. Uniforms adds 'model' to the props, which we use. */
 ClubPage.propTypes = {
-  clubs: PropTypes.array.isRequired,
+  doc: PropTypes.object,
+  model: PropTypes.object,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
+export default withTracker(({ match }) => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const documentId = match.params._id;
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe('Clubs');
   return {
-    clubs: Clubs.find({}).fetch(),
+    doc: Clubs.findOne(documentId),
     ready: subscription.ready(),
   };
 })(ClubPage);
