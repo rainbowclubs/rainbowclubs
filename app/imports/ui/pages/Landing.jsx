68d52152526d2a3aac1369e291d320faca9bce00
acathers <37058209+acathers@users.noreply.github.com>
2019-05-01 21:37:44 -1000

    issue-33-update

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -19,9 +19,12 @@ class Landing extends React.Component {
   }
 
   renderUser() {
+    const preferredClubs = this.props.clubs.filter(function (club) {
+      return club.types.some(value => Meteor.user().profile.types.includes(value));
+    });
     const randomClubs = [];
     for (let i = 0; i < 3; i++) {
-      randomClubs.push(this.props.clubs[Math.floor(Math.random() * this.props.clubs.length)]);
+      randomClubs.push(preferredClubs[Math.floor(Math.random() * preferredClubs.length)]);
     }
     const isAdmin = Roles.userIsInRole(Meteor.userId(), 'admin');
     const isModerator = Roles.userIsInRole(Meteor.userId(), 'moderator');
@@ -73,10 +76,10 @@ class Landing extends React.Component {
                           label={{ basic: true, pointing: 'right', content: pendingReviews }}
                           labelPosition='left'
                           as={Link} to='moderate'/></Grid.Column>
-              <Grid.Column>
+                <Grid.Column>
                   <Button position='right' size='huge' labeled='true'
                           className='UHGreenBackground UHGreenBackgroundHover UHWhiteTextColor'
-                          content='Modifcation Reviews'
+                          content='Modification Reviews'
                           label={{ basic: true, pointing: 'right', content: pendingModifications }}
                           labelPosition='left'
                           as={Link} to='moderate'/>
@@ -84,7 +87,7 @@ class Landing extends React.Component {
               </Grid>
             </Container>
           </Container>
-    );
+      );
     }
     if (isAdmin) {
       adminSection = (
@@ -99,102 +102,102 @@ class Landing extends React.Component {
       );
     }
     return (
-      <div>
-        <Container>
-          <Header className='UHGreenTextColor' as="h1" textAlign="center">Random Clubs</Header>
-          <Card.Group>
-            {randomClubs.map((club, index) => <Club key={index} club={club}/>)}
-          </Card.Group>
-          <Grid columns={2}>
-            <Grid.Column verticalAlign='middle' textAlign='right'>
-              <Header className='UHGreenTextColor' as="h2">Want to view more clubs?</Header>
-            </Grid.Column>
-            <Grid.Column verticalAlign='middle' textAlign='left'>
-              <Button size='huge' className='UHGreenBackground UHGreenBackgroundHover UHWhiteTextColor'
-                      as={Link} to='list'>View All Clubs</Button>
-            </Grid.Column>
-          </Grid>
-        </Container>
-        {clubAdminSection}
-        {moderatorSection}
-        {adminSection}
-      </div>
+        <div>
+          <Container>
+            <Header className='UHGreenTextColor' as="h1" textAlign="center">Clubs Matching Your Interests</Header>
+            <Card.Group>
+              {randomClubs.map((club, index) => <Club key={index} club={club}/>)}
+            </Card.Group>
+            <Grid columns={2}>
+              <Grid.Column verticalAlign='middle' textAlign='right'>
+                <Header className='UHGreenTextColor' as="h2">Want to view more clubs?</Header>
+              </Grid.Column>
+              <Grid.Column verticalAlign='middle' textAlign='left'>
+                <Button size='huge' className='UHGreenBackground UHGreenBackgroundHover UHWhiteTextColor'
+                        as={Link} to='list'>View All Clubs</Button>
+              </Grid.Column>
+            </Grid>
+          </Container>
+          {clubAdminSection}
+          {moderatorSection}
+          {adminSection}
+        </div>
     );
-    }
+  }
 
-    renderNotLoggedIn() {
-      return (
-      <div>
-      <Container textAlign='center' className={'LandingBackgroundImage'} fluid>
-      <Grid style={{ height: '100%', paddingBottom: '30px' }} verticalAlign='bottom' columns={2}>
-      <Grid.Row>
-      <Grid.Column>
-      <Header className="huge UHWhiteTextColor">Every club under the rainbow!</Header>
-      </Grid.Column>
-      <Grid.Column>
-      <Header className="huge UHWhiteTextColor">Which one will you choose?</Header>
-      </Grid.Column>
-      </Grid.Row>
-      </Grid>
-      </Container>
-      <Container fluid style={{ paddingTop: '20px' }}>
-      <Grid columns={'equal'} padded>
-      <Grid.Column>
-      <Segment>
-      <Header textAlign='center' className='UHGreenTextColor' as='h2'>Accounting Club</Header>
-      </Segment>
-      <Image centered fluid src='images/club1.png'/>
-      </Grid.Column>
-      <Grid.Column>
-      <Segment>
-      <Header textAlign='center' className='UHGreenTextColor' as='h2'>Pre-Vet Club</Header>
-      </Segment>
-      <Image centered fluid src='images/club2.png'/>
-      </Grid.Column>
-      <Grid.Column>
-      <Segment>
-      <Header textAlign='center' className='UHGreenTextColor' as='h2'>Katipunan Club</Header>
-      </Segment>
-      <Image centered fluid src='images/club3.png'/>
-      </Grid.Column>
-      </Grid>
-      </Container>
-      <Container>
-      <Grid columns={2} centered>
-      <Grid.Column verticalAlign='middle' textAlign={'center'}>
-      <Segment>
-      <Header className='UHGreenTextColor' as={'h2'}>Easily browse the registered clubs
-      of UH Manoa by
-      name and
-      category!
-      </Header>
-      </Segment>
-      </Grid.Column>
-      <Grid.Column>
-      <Image src={'images/clubs.jpg'}/>
-      </Grid.Column>
-      </Grid>
-      </Container>
-      </div>
-      );
-    }
-    }
+  renderNotLoggedIn() {
+    return (
+        <div>
+          <Container textAlign='center' className={'LandingBackgroundImage'} fluid>
+            <Grid style={{ height: '100%', paddingBottom: '30px' }} verticalAlign='bottom' columns={2}>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header className="huge UHWhiteTextColor">Every club under the rainbow!</Header>
+                </Grid.Column>
+                <Grid.Column>
+                  <Header className="huge UHWhiteTextColor">Which one will you choose?</Header>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+          <Container fluid style={{ paddingTop: '20px' }}>
+            <Grid columns={'equal'} padded>
+              <Grid.Column>
+                <Segment>
+                  <Header textAlign='center' className='UHGreenTextColor' as='h2'>Accounting Club</Header>
+                </Segment>
+                <Image centered fluid src='images/club1.png'/>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment>
+                  <Header textAlign='center' className='UHGreenTextColor' as='h2'>Pre-Vet Club</Header>
+                </Segment>
+                <Image centered fluid src='images/club2.png'/>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment>
+                  <Header textAlign='center' className='UHGreenTextColor' as='h2'>Katipunan Club</Header>
+                </Segment>
+                <Image centered fluid src='images/club3.png'/>
+              </Grid.Column>
+            </Grid>
+          </Container>
+          <Container>
+            <Grid columns={2} centered>
+              <Grid.Column verticalAlign='middle' textAlign={'center'}>
+                <Segment>
+                  <Header className='UHGreenTextColor' as={'h2'}>Easily browse the registered clubs
+                    of UH Manoa by
+                    name and
+                    category!
+                  </Header>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Image src={'images/clubs.jpg'}/>
+              </Grid.Column>
+            </Grid>
+          </Container>
+        </div>
+    );
+  }
+}
 
-      /** Require an array of Club documents in the props. */
-    Landing.propTypes = {
-      clubs: PropTypes.array.isRequired,
-      reviews: PropTypes.array.isRequired,
-      ready: PropTypes.bool.isRequired,
-    };
+/** Require an array of Club documents in the props. */
+Landing.propTypes = {
+  clubs: PropTypes.array.isRequired,
+  reviews: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
 
-      /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-    export default withTracker(() => {
-      // Get access to Stuff documents.
-      const clubSubscription = Meteor.subscribe('Clubs');
-      const reviewSubscription = Meteor.subscribe('ReviewsModerator');
-      return {
-      clubs: Clubs.find({}).fetch(),
-      reviews: Reviews.find({}).fetch(),
-      ready: clubSubscription.ready() && reviewSubscription.ready(),
-    };
-    })(Landing);
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const clubSubscription = Meteor.subscribe('Clubs');
+  const reviewSubscription = Meteor.subscribe('ReviewsModerator');
+  return {
+    clubs: Clubs.find({}).fetch(),
+    reviews: Reviews.find({}).fetch(),
+    ready: clubSubscription.ready() && reviewSubscription.ready(),
+  };
+})(Landing);
