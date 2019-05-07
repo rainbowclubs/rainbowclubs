f14a01b1ea5928a78088b6158b861392677d9916
acathers <37058209+acathers@users.noreply.github.com>
2019-05-06 17:05:46 -1000

    issue-32-update

---
+++ app/imports/ui/components/ClubReviewItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Table } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Link, withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class ClubReviewItem extends React.Component {
+
+  convertBoolToYesNo(value) {
+    return value ? 'Yes' : 'No';
+  }
+
+  render() {
+    return (
+        <Table.Row>
+          <Table.Cell>{(this.props.club.name)}</Table.Cell>
+          <Table.Cell>{this.convertBoolToYesNo(this.props.club.reviewed)}</Table.Cell>
+          <Table.Cell><Link to={`/moderatereview/${this.props.review._id}`}>View</Link></Table.Cell>
+        </Table.Row>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+ClubReviewItem.propTypes = {
+  club: PropTypes.object.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(ClubReviewItem);
---
+++ app/imports/ui/pages/ModeratorListClubs.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Table, Header, Loader, Popup } from 'semantic-ui-react';
+import ReviewItem from '/imports/ui/components/ReviewItem';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Reviews } from '../../api/review/review';
+
+/** Renders a table containing all of the Review documents. Use <ReviewItem> to render each row. */
+class ModeratorListClubs extends React.Component {
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  /** Render the page once subscriptions have been received. */
+  renderPage() {
+    return (
+        <Container>
+          <Header as="h2" textAlign="center">Pending Reviews</Header>
+          <Table className='UHGreenShadow' celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>
+                  <Popup on='hover' trigger={<Header as='h4'>Contents</Header>}
+                         content='Review contents'/>
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <Popup on='hover' trigger={<Header as='h4'>Flagged</Header>}
+                         content='Review has been flagged for review'/>
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <Popup on='hover' trigger={<Header as='h4'>Reviewed</Header>} content='Review has been reviewed'/>
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <Popup on='hover' trigger={<Header as='h4'>Visible</Header>} content='Review is visible'/>
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <Popup on='hover' trigger={<Header as='h4'>Review</Header>}
+                         content='Click to view review'/>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {this.props.clubs.map((review) => <ReviewItem key={review._id} review={review}/>)}
+            </Table.Body>
+          </Table>
+        </Container>
+    );
+  }
+}
+
+/** Require an array of Review documents in the props. */
+ModeratorListClubs.propTypes = {
+  clubs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Review documents.
+  const subscription = Meteor.subscribe('ClubsReview');
+  return {
+    clubs: Reviews.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(ModeratorListClubs);
